9150757c0036a6424b757cdb8571dbafd6c85107
Danh Vuong <danhvuong@Danhs-Macbook.local>
2019-06-12 07:32:50 -0700

    working on graphic

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 var ctx = document.getElementById('ctx').getContext('2d');
-const WIDTH = 500;
-const HEIGHT = 500;
-var player ={
-    x: 250,
-    y: 450,
-    width: 20,
-    height: 60,
-    color: "green",
-    atkSpd: 3
-}
+const WIDTH = 440;
+const HEIGHT = 680;
+var Img = {};
+Img.player = new Image();
+Img.player.src = "img/player.png";
+Img.enemy1 = new Image();
+Img.enemy1.src = "img/enemy1.png";
+Img.enemy2 = new Image();
+Img.enemy2.src = "img/enemy2.png";
+Img.enemy3 = new Image();
+Img.enemy3.src = "img/enemy3.png";
+Img.boss = new Image();
+Img.boss.src = "img/boss.png";
+Img.bullet = new Image();
+Img.bullet.src = "img/beams.png";
+Img.bg = new Image();
+Img.bg.src = "img/space.png";
+
 var timeWhenStart = Date.now();
 var bulletList ={};
 var enemyBulletList = {};
@@ -22,7 +30,18 @@ var frame = 0;
 var isBoss = false;
 var timeUpgrading = 900;
 
-function createEnemy(xSpeed,ySpeed,x,y,width,height,color,hp,id){
+var player ={
+    x: 250,
+    y: 450,
+    width: 80,
+    height: 80,
+    color: "green",
+    atkSpd: 3,
+    img: Img.player
+}
+ctx.fillText("Hello",250,250);
+
+function createEnemy(xSpeed,ySpeed,x,y,width,height,color,hp,img,id){
     var enemy = {
         xSpeed: xSpeed,
         ySpeed: ySpeed,
@@ -32,12 +51,13 @@ function createEnemy(xSpeed,ySpeed,x,y,width,height,color,hp,id){
         height: height,
         color: color,
         hp : hp,
+        img : img,
         id: id
     }
     enemyList[id] = enemy;
 }
 
-function createBoss(xSpeed,ySpeed,x,y,width,height,color,hp,atkSpd,id){
+function createBoss(xSpeed,ySpeed,x,y,width,height,color,hp,atkSpd,img,id){
     var boss = {
         xSpeed: xSpeed,
         ySpeed: ySpeed,
@@ -48,6 +68,7 @@ function createBoss(xSpeed,ySpeed,x,y,width,height,color,hp,atkSpd,id){
         color: color,
         hp : hp,
         atkSpd : atkSpd,
+        img: img,
         id: id
     }
     bossList[id] = boss;
@@ -55,15 +76,16 @@ function createBoss(xSpeed,ySpeed,x,y,width,height,color,hp,atkSpd,id){
 function boss1(){
     var xSpeed = -2 + Math.random()*3;
     var ySpeed = 2;
-    var x = 250;
+    var x = WIDTH/2;
     var y = 0;
-    var width = 100;
-    var height = 30;
+    var width = 150;
+    var height = 150;
     var color = "pink";
     var hp = 100 + level*2;
     var atkSpd = -2 - level;
+    var img = Img.boss;
     var id = Math.random()
-    createBoss(xSpeed,ySpeed,x,y,width,height,color,hp,atkSpd,id);
+    createBoss(xSpeed,ySpeed,x,y,width,height,color,hp,atkSpd,img,id);
 }
 
 function randomEnemy1(){
@@ -71,36 +93,39 @@ function randomEnemy1(){
     var ySpeed = 1+ Math.random()*3;
     var x = Math.random()*WIDTH;
     var y = 0;
-    var width = 60;
-    var height = 20;
+    var width =80;
+    var height = 80;
     var color = "red";
     var hp = 1 + level;
+    var img = Img.enemy1;
     var id = Math.random()
-    createEnemy(xSpeed,ySpeed,x,y,width,height,color,hp,id);
+    createEnemy(xSpeed,ySpeed,x,y,width,height,color,hp,img,id);
 }
 function randomEnemy2(){
     var xSpeed = -2 + Math.random()*3;
     var ySpeed = 1+ Math.random()*3;
     var x = Math.random()*WIDTH;
     var y = 0;
-    var width = 60;
-    var height = 20;
+    var width = 80;
+    var height = 80;
     var color = "blue";
     var hp = 3+level;
+    var img = Img.enemy2;
     var id = Math.random()
-    createEnemy(xSpeed,ySpeed,x,y,width,height,color,hp,id);
+    createEnemy(xSpeed,ySpeed,x,y,width,height,color,hp,img,id);
 }
 function randomEnemy3(){
     var xSpeed = 1;
     var ySpeed = 1;
     var x = Math.random()*WIDTH;
     var y = 0;
-    var width = 80;
-    var height = 20;
+    var width = 100;
+    var height = 100;
     var color = "orange";
     var hp = 20 + level;
+    var img = Img.enemy3;
     var id = Math.random()
-    createEnemy(xSpeed,ySpeed,x,y,width,height,color,hp,id);
+    createEnemy(xSpeed,ySpeed,x,y,width,height,color,hp,img,id);
 }
 function createBullet(ySpeed,xSpeed,x,y,height,width,color,id){
     var bullet = {
@@ -134,7 +159,7 @@ function generateBullet(shooter,side,angle){
     var y = shooter.y;
     var height = 10;
     var width = 4;
-    var color = "black";
+    var color = "white";
     var id = Math.random();
     createBullet(ySpeed,xSpeed,x,y,height,width,color,id);
 }
@@ -144,15 +169,16 @@ function generateEnemyBullet(shooter){
     var y = shooter.y;
     var height = 10;
     var width = 4;
-    var color = "black";
+    var color = "white";
     var id = Math.random();
     createEnemyBullet(ySpeed,x,y,height,width,color,id);
 }
 
-function createBonus(x,y,width,height,color,category,id){
+function createBonus(x,y,ySpeed,width,height,color,category,id){
     var bonus ={
-        x:y,
+        x:x,
         y:y,
+        ySpeed:ySpeed,
         width: width,
         height: height,
         color: color,
@@ -161,13 +187,14 @@ function createBonus(x,y,width,height,color,category,id){
     }
     bonusList[id] = bonus;
 }
-function generateBonus(){
-    var x = Math.random()*WIDTH;
-    var y = Math.random()*HEIGHT;
+function generateBonus(mob){
+    var x = mob.x;
+    var y = mob.y;
+    var ySpeed = 3;
     var width = 10;
     var height = 10;
     var id = Math.random();
-    if(Math.random()<0.5){
+    if(Math.random()<0.8){
         var category = 'score';
         var color = 'orange';
     } 
@@ -175,31 +202,30 @@ function generateBonus(){
             var category = 'atkSpd';
             var color = 'purple';
     }
-    createBonus(x,y,width,height,color,category,id);
+    createBonus(x,y,ySpeed,width,height,color,category,id);
 }
 
-
+function drawImageActor(actor){
+    ctx.save();
+    var x = actor.x-actor.width/2;
+    var y = actor.y-actor.height/2
+    // ctx.drawImage(actor.img,x,y)
+    ctx.drawImage(actor.img,0,0,actor.img.width,actor.img.height,x,y,actor.width,actor.height)
+    ctx.restore();
+}
 function drawActor(actor){
     ctx.save();
     ctx.fillStyle = actor.color;
     ctx.fillRect(actor.x-actor.width/2,actor.y-actor.height/2,actor.width,actor.height);
     ctx.restore();
 }
-//  function updateChasingEnemyPosition(enemy){
-//         enemy.x += enemy.xSpeed;
-//         enemy.y += enemy.ySpeed;
-//         var distX = enemy.x - player.x;
-//         var distY = enemy.y - player.y;
-//         if(distX > 0){
-//             enemy.x -= enemy.xSpeed+1;
-//         }
-//         else if(distX <= 0){
-//             enemy.x += enemy.xSpeed;
-//         }
-//         if(distY > 0){
-//             enemy.y -= enemy.ySpeed;
-//         }
-//  }
+function updateBonusPosition(bonus){
+    bonus.y += bonus.ySpeed;
+}
+function updateBonus(bonus){
+    drawActor(bonus);
+    updateBonusPosition(bonus);
+}
  function updateRandomEnemyPosition(enemy){
         enemy.x += enemy.xSpeed;
         enemy.y += enemy.ySpeed;
@@ -226,12 +252,9 @@ function updateBossBullet(bullet){
     drawActor(bullet);
     updateBossBulletPosition(bullet);
 }
- function updateChasingEnemy(enemy){
-     drawActor(enemy);
-     updateChasingEnemyPosition(enemy);
- }
+ 
  function updateRandomEnemy(enemy){
-     drawActor(enemy);
+     drawImageActor(enemy);
      updateRandomEnemyPosition(enemy);
  }
 
@@ -279,8 +302,8 @@ testCollisionEntity = function (entity1,entity2){       //return if colliding (t
 
 
 update = function(){
-    ctx.clearRect(0,0,500,500);
-    
+    ctx.clearRect(0,0,WIDTH,HEIGHT);
+    ctx.drawImage(Img.bg,0,0);
     frame++ 
     var timePlaying = Date.now()-timeWhenStart;
     if( frame % 1800 === 0){
@@ -332,9 +355,9 @@ update = function(){
             timeUpgrading = 900;
         }
     }
-    if(frame % 500 === 0){
-        generateBonus();
-    }
+    // if(frame % 500 === 0){
+    //     generateBonus();
+    // }
     for(var key in enemyList){
         updateRandomEnemy(enemyList[key]);
         var isCollision = testCollisionEntity(player,enemyList[key]);
@@ -352,12 +375,29 @@ update = function(){
                 enemyList[i].hp -= 1
                 delete bulletList[key];
                 if(enemyList[i].hp === 0){
+                    generateBonus(enemyList[i]);
                     delete enemyList[i];
                     score += 100;
                 }
                 break;
             }
         }   
+    }
+    for(var key3 in bonusList){
+        updateBonus(bonusList[key3]);
+        var isCollision = testCollisionEntity(player,bonusList[key3]);
+        if(isCollision){
+            if(bonusList[key3].category === 'score')
+                score +=100;
+            if(bonusList[key3].category === 'atkSpd'){
+                player.atkSpd += 2;
+                if(player.atkSpd > 21){
+                    player.atkSpd =21;
+                    timeUpgrading += 300;
+                }
+            }
+            delete bonusList[key3];
+        }
     }
     for(var key in bulletList){
         for(var key2 in bossList){
@@ -376,30 +416,17 @@ update = function(){
         }
     }
     
-    for(var key in bonusList){
-        drawActor(bonusList[key]);
-        var isCollision = testCollisionEntity(player,bonusList[key]);
-        if(isCollision){
-            if(bonusList[key].category === 'score')
-                score +=1000;
-            if(bonusList[key].category === 'atkSpd'){
-                player.atkSpd += 2;
-                if(player.atkSpd > 21){
-                    player.atkSpd =21;
-                    timeUpgrading += 300;
-                }
-            }
-            delete bonusList[key];
-        }
-    }
-
-    drawActor(player);
+    
+    drawImageActor(player);
     requestAnimationFrame(update);
-    ctx.font = "20px Georgia";
-    ctx.fillText("Score :" +score,50,50)
-    ctx.fillText("Level :" + level, 400,50);
+    ctx.font = "15px Georgia";
+    ctx.fillText("Score :" +score,50,20);
+    ctx.fillText("Level :" + level, 350,20);
 }
-
+function drawImg(actor){
+    
+    ctx.drawImage(actor.img,0,0);
+}
 
 update();
 
